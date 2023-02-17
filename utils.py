9eb417ed958f0220e6c57d7380f88517eca665b7
yazidsaiad <yazid.saiad@tnpconsultants.com>
2023-02-17 11:01:44 +0100

    add chromedriver path

--- utils.py
+++ utils.py
@@ -1,8 +1,8 @@
 import pandas as pd
 from selenium import webdriver
 # from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.chrome.service import Service
+# from webdriver_manager.chrome import ChromeDriverManager
+# from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from io import BytesIO
 from selenium.webdriver.chrome.options import Options
