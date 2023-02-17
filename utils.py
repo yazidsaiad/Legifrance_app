59ff7662b0d6719fb8077f429cc76bdb7f2df7c9
yazidsaiad <yazid.saiad@tnpconsultants.com>
2023-02-17 13:08:24 +0100

    removed headless from chrome options

--- utils.py
+++ utils.py
@@ -22,7 +22,7 @@ def scrap_articles():
 
     
     options = Options()     # chrome options for the webdrivers
-    options.add_argument("--headless")
+    # options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument('--no-sandbox')     
     options.add_argument('--disable-dev-shm-usage')        
