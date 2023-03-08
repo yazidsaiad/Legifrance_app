e2861aff870d1c68ac5c28453cb2f652c0ff3452
yazidsaiad <yazid.saiad@tnpconsultants.com>
2023-03-08 23:23:10 +0100

    st.cache on again

--- utils.py
+++ utils.py
@@ -26,7 +26,7 @@ def chunck_list(list_ : list, batch_size : int):
 
     return chuncked_list
 
-# @st.cache_data
+@st.cache_data
 def get_ids_and_names():
 
     """
@@ -73,7 +73,7 @@ def get_ids_and_names():
 
     return IDS, NAMES
 
-# @st.cache_data
+@st.cache_data
 def get_articles(ids : list, timeout : int):
     """
     Allow the legislative and regulatory texts scraping on Légifrance website.
